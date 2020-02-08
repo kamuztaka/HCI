68e9165b35abe121499f0ba6c090ae0b34a8a161
Schultz Henry Boado <57819354+kamuztaka@users.noreply.github.com>
2020-02-08 18:24:09 +0800

    Add files via upload

---
+++ AIMS/Login/login.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#login').click(function(){
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'You are Login',
+            showConfirmButton: true,
+        })
+    });
+});
